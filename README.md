4d8e2e7a70a3af61bb701eaf6429810d709fb398
Shariqbinrashid <shariqbinrashid@gmail.com>
2020-12-09 03:12:23 +0500

    Sign In

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Hopload-Civic-Hackathon-2020-Team-ORION
 
 This project is for Civic Hackathon 2020 by Code For Pakistan from Team ORION.
+protoype:
+ https://www.figma.com/file/2NKSJ8SkNphQF3fIec9HNX/iOS-APP-Patient-Only
+ https://www.figma.com/proto/98CCQJ0QXjlg9KL00NIDDe/hoploud?node-id=0%3A1&scaling=scale-down
+
+ Document:
+ https://docs.google.com/document/d/1CAmSPAH_jrnbPEnnXcmnVG_jcLM7ze7lkECsAeRbg9A/edit?usp=sharing
---
+++ src/components/SignInComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+
+
+class SignIn extends Component {
+
+    constructor(props) {
+        super(props);
+     
+    }
+
+    render() {
+       
+        return (
+            <div>
+               <h1> Sign IN</h1>
+            </div>
+        );
+    }
+}
+
+
+export default SignIn;
